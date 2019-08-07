7342337438afcf0689e6bce1f068e5bc8f1d94fc
Rahul Nayak <rahulnayak@electroons.com>
2019-08-07 12:35:43 +0530

    added signInModal.js component

--- src/Component/SignUpModal.js
+++ src/Component/SignUpModal.js
@@ -9,7 +9,7 @@ class SignUpModal extends React.Component {
     return (
       <Modal
         className="sign-up-modal"
-          isOpen={this.props.isModalOpen}
+          isOpen={this.props.isModalOpen} 
           contentLabel="Modal for Sign up form"
         >
           <form>
@@ -17,10 +17,12 @@ class SignUpModal extends React.Component {
           <input type="text" id="username" placeholder="Add a unique username" required />
           <h3 className="email-id">Email-id:</h3>
           <input type="email" id="email-id" placeholder="Add your email id" required />
-          <h3 className="password">Email-id:</h3>
+          <h3 className="password">Password:</h3>
           <input type="passoword" id="password" placeholder="Keep a strong password" required />
           <button onSubmit={this.props.closeModal}>Sign me up!</button>
           </form>
+
+          <p>Already a member? <a href="#">Sign in here</a></p>
            </Modal>
     )
   }
