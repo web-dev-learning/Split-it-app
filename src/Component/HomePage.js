b6e32d77d0f94c330e6d74a5d2f0563bd337ee4c
Rahul Nayak <rahulnayak@electroons.com>
2019-08-23 12:06:19 +0530

    addedd prop-type library, indented Homepage.js component and added seperate state for login, signup

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -2,34 +2,42 @@ import React from "react";
 import logo from "../images/Split-it-logo.png";
 import SignUpModal from "./SignUpModal";
 import SignInModal from "./SignInModal";
+import PropTypes from "prop-types";
 
 class HomePage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isModalOpen: false
-        };
-        this.handleModalWindow = this.handleModalWindow.bind(this);
-    }
-      handleModalWindow = e => {
-        e.preventDefault();
-        this.setState(() => ({ isModalOpen: !this.state.isModalOpen }));
-      };
+			isSignUpModalOpen: false,
+			isLogInModalOpen: false
+		};
+		this.handleSignUpModal = this.handleSignUpModal.bind(this);
+		this.handleLogInModal = this.handleLogInModal.bind(this);
+	}
+	handleSignUpModal = (e) => {
+		e.preventDefault();
+		this.setState((state) => ({isSignUpModalOpen: !state.isSignUpModalOpen}));
+	};
+	handleLogInModal = (e) => {
+		e.preventDefault();
+		this.setState((state) => ({isLogInModalOpen: !state.isLogInModalOpen}));
+	};
 	render() {
 		return (
 			<div>
 				<img src={logo} alt="Spli-it app logo" />
-				<button onClick={this.handleModalWindow}>Log-in</button>
-				<button onClick={this.handleModalWindow}>Sign-up</button>
-                <SignUpModal 
-                openModal={this.state.isModalOpen} 
-                closeModal={this.handleModalWindow} />
-                <SignInModal   
-                openModal={this.state.isModalOpen} 
-                closeModal={this.handleModalWindow} />
+				<button onClick={this.handleLogInModal}>Log-in</button>
+				<button onClick={this.handleSignUpModal}>Sign-up</button>
+				<SignUpModal openModal={this.state.isSignUpModalOpen} closeModal={this.handleSignUpModal} />
+				<SignInModal openModal={this.state.isLogInModalOpen} closeModal={this.handleLogInModal} />
 			</div>
 		);
 	}
 }
+
+HomePage.propTypes = {
+	isSignUpModalOpen: PropTypes.bool,
+	isLogInModalOpen: PropTypes.bool
+};
 
 export default HomePage;
